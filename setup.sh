22709905b661ccbfe19f0a9003b7ff82b6faa484
Brandon Currell <brandon+git@currell.pw>
2026-02-12 02:46:17 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,3 +3,14 @@
 ###
 # Setup script for https://github.com/BCurrell/dotfiles-sysadmin
 ###
+
+# Remove any existing config files that we have versions for:
+rm -rf \
+  ~/.bash* \
+  ~/.profile*
+
+# Create symlinks for our config files
+ln -sn ~/dotfiles/profile ~/.profile
+ln -sn ~/dotfiles/profile.d ~/.profile.d
+ln -sn ~/dotfiles/bashrc ~/.bashrc
+ln -sn ~/dotfiles/bashrc.d ~/.bashrc.d
